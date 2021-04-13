1a85252097fb43d876b6ba4c8957d9251543abb6
Advaith <advaithj1@gmail.com>
2021-04-13 12:15:15 -0700

    dont embed help links

--- index.ts
+++ index.ts
@@ -62,12 +62,12 @@ client.ws.on('INTERACTION_CREATE', async (interaction: APIInteraction) => {
     } else if (interaction.data.id === commandIDs.help) {
         respond({
             content: stripIndent`
-                Slashtags is a simple [slash command](https://support.discord.com/hc/en-us/articles/1500000368501) tag bot.
+                Slashtags is a simple [slash command](<https://support.discord.com/hc/en-us/articles/1500000368501>) tag bot.
                 • Use /create to create a tag, /edit to edit a tag, and /delete to delete a tag.
                 • All created tags will show when a user types /, making them easy to discover.
                 • The Manage Server permission is required to manage tags.
                 • Due to Discord limits, you can create up to 100 Slashtags per server. Slash commands may not show in servers with over 50 bots.
-                Created by [advaith](https://advaith.io) • [Add to your server](https://discord.com/api/oauth2/authorize?client_id=790910161953882147&scope=bot+applications.commands) • [Privacy Policy](https://gist.github.com/advaith1/6fd1ad3ed1ad30304ba97528f5561935)`
+                Created by [advaith](<https://advaith.io>) • [Add to your server](<https://discord.com/api/oauth2/authorize?client_id=790910161953882147&scope=bot+applications.commands>) • [Privacy Policy](<https://gist.github.com/advaith1/6fd1ad3ed1ad30304ba97528f5561935>)`
         })
 
     } else if (interaction.data.id === commandIDs.create) {
